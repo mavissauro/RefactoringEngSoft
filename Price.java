a005996f4c2c9da9ba4ca9447a01864506e7a986
Marcos V. Ribeiro <marcos.ribeiro@syngenta.com>
2022-02-04 09:11:40 -0300

    Commit 12

--- Price.java
+++ Price.java
@@ -1,3 +1,25 @@
 public abstract class Price {
     public abstract int getPriceCode();
+
+    public double getCharge(int daysRented) {
+        double amount = 0;
+        
+        switch (this.getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (daysRented > 2)
+                    amount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount += 1.5;
+                if (daysRented > 3)
+                    amount += (daysRented - 3) * 1.5;
+                break;
+        }
+
+        return amount;
+    }
 }
