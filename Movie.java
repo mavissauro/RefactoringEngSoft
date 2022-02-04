56b2051a684d50843cb5ad97888e216fd0d66f8a
Marcos V. Ribeiro <marcos.ribeiro@syngenta.com>
2022-02-04 00:42:13 -0300

    Commit 9

--- Movie.java
+++ Movie.java
@@ -23,4 +23,26 @@ public class Movie {
     public String getTitle() {
         return _title;
     }
+
+    public double getCharge(int daysRented) {
+        double amount = 0;
+        
+        switch (this.getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (daysRented > 2)
+                    amount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount += 1.5;
+                if (daysRented > 3)
+                    amount += (daysRented - 3) * 1.5;
+                break;
+        }
+
+        return amount;
+    }
 }
--- Rental.java
+++ Rental.java
@@ -17,25 +17,7 @@ public class Rental {
     }
 
     public double getCharge() {
-        double amount = 0;
-        
-        switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                if (this.getDaysRented() > 2)
-                    amount += (this.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                amount += this.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                amount += 1.5;
-                if (this.getDaysRented() > 3)
-                    amount += (this.getDaysRented() - 3) * 1.5;
-                break;
-        }
-
-        return amount;
+        return _movie.getCharge(_daysRented);
     }
 
     public int getFrequentRenterPoints(){
