49902138fdc3f0245c1e001647fe865da60898a4
Marcos V. Ribeiro <marcos.ribeiro@syngenta.com>
2022-02-04 10:49:18 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,20 +1,4 @@
-import java.util.Enumeration;
-
-public class HtmlStatement extends Statement { 
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = getHeader(aCustomer);
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            //show figures for each rental
-            result += getFigure(each);
-        }
-        //add footer lines
-        result += getOwned(aCustomer);
-        result += getEarned(aCustomer);
-        return result;
-    }
-
+public class HtmlStatement extends Statement {
     public String getHeader(Customer aCustomer) {
         return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
     }
--- Statement.java
+++ Statement.java
@@ -1,3 +1,25 @@
-public class Statement {
+import java.util.Enumeration;
 
+public abstract class Statement {
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = getHeader(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            //show figures for each rental
+            result += getFigure(each);
+        }
+        //add footer lines
+        result += getOwned(aCustomer);
+        result += getEarned(aCustomer);
+        return result;
+    }
+
+    public abstract String getHeader(Customer aCustomer);
+
+    public abstract String getFigure(Rental aRental);
+
+    public abstract String getOwned(Customer aCustomer);
+
+    public abstract String getEarned(Customer aCustomer);
 }
